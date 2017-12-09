14a5c3f12e60718f60f8cda28bd826337aed6089
Lindsey Stanek <lstanek4@gmail.com>
2017-12-09 16:31:02 -0500

    Updates 2017

---
+++ app/js/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  //Controlling the hover states for the overlays under "Work"
+    $('#portfolio .col-one-third').mouseenter(function() {
+      $(this).children('.overlay').fadeIn('fast');
+    }).mouseleave(function() {
+      $(this).children('.overlay').fadeOut('fast');
+    });
+
+  //Form submit
+  	var form = $('#ajax-contact');
+  	var formMessages = $('#form-messages');
+
+  	// Set up an event listener for the contact form.
+  	$('button').click(function(event) {
+  		event.preventDefault();
+  		var formData = $(form).serialize();
+
+  		// Submit the form using AJAX.
+  		$.ajax({
+  			type: 'POST',
+  			url: $(form).attr('action'),
+  			data: formData
+  		})
+  		.done(function(response) {
+  			$(formMessages).text(response);
+  			$('#name, #email, #message').val('');
+
+        $('#form-messages').addClass('success');
+        $('#form-messages').removeClass('error');
+  		})
+  		.fail(function(data) {
+        $('#form-messages').addClass('error');
+        $('#form-messages').removeClass('success');
+  			if (data.responseText !== '') {
+  				$(formMessages).text(data.responseText);
+  			} else {
+  				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+  			}
+  		});
+  	});
+
+});
--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  //Controlling the hover states for the overlays under "Work"
-    $('#portfolio .col-one-third').mouseenter(function() {
-      $(this).children('.overlay').fadeIn('fast');
-    }).mouseleave(function() {
-      $(this).children('.overlay').fadeOut('fast');
-    });
-});
